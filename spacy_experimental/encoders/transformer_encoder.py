a5cab5eaa9d5e0fab0e9fff789452b263fcc8875
Daniël de Kok <me@danieldk.eu>
2022-02-16 17:15:01 +0100

    Add SinusoidalEncoding class
    Not wired up for now...

--- spacy_experimental/encoders/transformer_encoder.py
+++ spacy_experimental/encoders/transformer_encoder.py
@@ -1,3 +1,4 @@
+import math
 import torch
 
 from spacy import registry
@@ -84,6 +85,32 @@ class AbsPosEnc(nn.Module):
         x = x + pos_emb
         return self.dropout(x)
 
+
+# Source: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+class SinusoidalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000, normalize=True):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+
+        if normalize == True:
+            l2 = torch.norm(pe, dim=-1)
+            pe /= l2.unsqueeze(-1)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
 
 class TransformerModel(nn.Module):
 
